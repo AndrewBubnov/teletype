ec81b15c8c2a656702d221701de0c49c3297f447
andrew <andrew.bubnov75@gmail.com>
2024-01-10 13:14:31 +0100

    Fix unread number displaying

--- src/actions/updateMessageIsRead.ts
+++ src/actions/updateMessageIsRead.ts
@@ -4,7 +4,7 @@ import { prisma } from '@/db';
 export const updateMessageIsRead = async (messageId: string) => {
 	const message = await prisma.message.findUnique({ where: { id: messageId } });
 	if (!message) return;
-	await prisma.message.update({
+	return prisma.message.update({
 		where: { id: messageId },
 		data: { isRead: true },
 	});
