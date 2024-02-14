cd453c0f842f87aeeb84e66b3ddf913a3503d0b5
andrew <andrew.bubnov75@gmail.com>
2024-02-14 15:02:35 +0100

    Fix several styles

--- src/app/chat-list/components/ChatWrapper.tsx
+++ src/app/chat-list/components/ChatWrapper.tsx
@@ -2,15 +2,17 @@
 
 import { useActiveChatStore, useIsWideModeStore } from '@/store';
 import { Chat } from '@/app/chat-list/[chatId]/components/Chat';
+import styles from '../chat.module.css';
 
 export const ChatWrapper = () => {
 	const activeChat = useActiveChatStore(state => state.activeChat);
 	const isWideMode = useIsWideModeStore(state => state.isWideMode);
 
 	if (!activeChat || !isWideMode) return null;
+
 	return (
 		<>
-			<div style={{ width: 1, background: 'rgb(70, 69, 69)', marginBlock: '1rem' }} />
+			<div className={styles.divider} />
 			<Chat chat={activeChat} />
 		</>
 	);
--- src/app/chat-list/page.tsx
+++ src/app/chat-list/page.tsx
@@ -2,8 +2,8 @@ import { ChatsList } from '@/app/chat-list/components/ChatsList';
 import { Header } from '@/app/chat-list/components/Header';
 import { getUser } from '@/prismaActions/getUser';
 import { FullScreenLoader } from '@/app/shared/components/FullScreenLoader';
-import styles from './chat.module.css';
 import { ChatWrapper } from '@/app/chat-list/components/ChatWrapper';
+import styles from './chat.module.css';
 
 export default async function ChatListPage() {
 	const user = await getUser();
