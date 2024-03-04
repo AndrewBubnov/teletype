556d1d844997a0dfc8735e4cbad7a65fc1ccb79c
andrew <andrew.bubnov75@gmail.com>
2024-03-04 13:58:52 +0100

    Drop message text on change chat in wide mode

--- src/app/chat-list/[chatId]/components/MessageInput/MessageInput.tsx
+++ src/app/chat-list/[chatId]/components/MessageInput/MessageInput.tsx
@@ -42,6 +42,10 @@ export const MessageInput = ({
 	useSendTyping(interlocutorId, messageText);
 
 	useEffect(() => {
+		setMessageText('');
+	}, [chatId]);
+
+	useEffect(() => {
 		if (!editedMessage) return;
 		setMessageImageUrl(editedMessage.imageUrl || '');
 		setMessageText(editedMessage.text || '');
